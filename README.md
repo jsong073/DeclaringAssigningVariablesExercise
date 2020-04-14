70bdc8954d3b6760b1e77e4f4e642a0e1f219040
jsong073 <jaehasong073@gmail.com>
2020-04-14 11:49:19 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Practice with Declaring and Assigning Variables" 
---
+++ src/DeclaringAssigningVariablesExercise.java
@@ -0,0 +1,11 @@
+public class DeclaringAssigningVariablesExercise {
+    public static void main (String[] args) {
+        int testScore = 86;
+        double price = 3.52;
+        String username = "melonlord99";
+
+        System.out.println("Test Score initialized is: " + testScore);
+        System.out.println("Price tag: " + price);
+        System.out.println("Username: " + username);
+    }
+}
